1dfd9e698d399314a248dd0e6925abe509c26e35
Joshua Fiel <Joshua@Joshuas-MBP.home>
2021-06-01 06:58:20 -0400

    sus coils

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -6,3 +6,21 @@ model <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clear
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
              ground_clearance + AWD, data = mpg))
 ##determine the p-value and the r-squared value for the linear regression model
+
+suspension_coil <- read.csv('Suspension_Coil.csv',stringsAsFactors = F) #read in dataset
+
+# create total_summary for PSI
+total_summary_PSI <- suspension_coil %>% 
+  summarize(Mean = mean(PSI), 
+            Median = median(PSI), 
+            Variance = var(PSI),
+            SD = sd(PSI),
+            .groups = "keep")
+
+# create total_summary for lot
+total_summary_lot <- suspension_coil %>% 
+  summarize(Mean = mean(Manufacturing_Lot), 
+            Median = median(Manufacturing_Lot), 
+            Variance = var(Manufacturing_Lot),
+            SD = sd(Manufacturing_Lot),
+            .groups = "keep")
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # MechaCar_Statistical_Analysis
+
+
+* Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+
+* Is the slope of the linear model considered to be zero? Why or why not?
+
+* Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+
+## Linear Regression to Predict MPG
+
+The vehicle_length, ground_clearance and intercept variables are likely to have provided a non-random amount variance to the mpg values in the data.
+
+Since the coefficients are not zero, the slope cannot be zero.
+
+This linear model does not predict mpg as as the statistical significance of the intercept indicates that there are other variables that need to be considered.
